3e6d1e1f0351acd2d4dcb14851d85859df81ba13
MohamedSarfaraz98 <111573255+MohamedSarfaraz98@users.noreply.github.com>
2022-10-04 17:49:03 +0530

    LabBook2

---
+++ CheckOddEven.java
@@ -0,0 +1,14 @@
+package LabBook2;
+
+public class CheckOddEven {
+public static void main(String[] args) {
+	int number=-1;
+	if(number%2==0) {
+		System.out.println("Even");
+	}
+	else {
+		System.out.println("Odd");
+	}
+	System.out.println("Bye");
+}
+}
---
+++ CheckPassFail.java
@@ -0,0 +1,14 @@
+package LabBook2;
+
+public class CheckPassFail {
+public static void main(String[] args) {
+	int mark=49;
+	if(mark>=50) {
+		System.out.println("Pass");
+	}
+	else {
+		System.out.println("Fail");
+	}
+	System.out.println("Done");
+}
+}
